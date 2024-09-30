d1def52d59567a991f52290a040feb00d01d33ba
mukeshgupta7 <39895994+mukeshgupta7@users.noreply.github.com>
2024-09-30 22:26:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const form = document.getElementById('campaign-form');
 const responseContainer = document.getElementById('response-container');
 const API_URL = 'http://18.191.132.231:5000';
-import axios from 'axios';
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
